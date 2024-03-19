ec46a0700da9621501b49beb66152966977ae05d
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-03-19 16:47:40 +0700

    🆙Update (ListBox): drilling prop w  ListBox component composition

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import WatchedBox from "./WatchedBox";
 import ListBox from "./ListBox";
+import MovieList from "./MovieList";
 
 const Main = ({ movies }) => {
   return (
     <main className="main">
-      <ListBox movies={movies} />
+      <ListBox movies={movies}>
+        <MovieList movies={movies} />
+      </ListBox>
       <WatchedBox />
     </main>
   );
