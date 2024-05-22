eed3baa96e670870fce0d9e5163d5825aeaa695f
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-22 21:17:54 +0700

    🆙update (index): usage rating component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
 import StarRating from "./StarRating";
+import TestRating from "./TestRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}/>
     <StarRating color="red" size="24"/>
+    <TestRating color="blue" size="32"/>
   </React.StrictMode>
 );
