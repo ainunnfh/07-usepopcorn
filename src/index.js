2982c829e486899770008348109de4314c2fa606
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-08-21 23:07:34 +0700

    💄update (index): usage app comp

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 import TestRating from "./TestRating";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}/>
+    {/* <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}/>
     <StarRating color="red" size="24" />
-    <TestRating size="32"/>
+    <TestRating size="32"/> */}
+    <App></App>
   </React.StrictMode>
 );
